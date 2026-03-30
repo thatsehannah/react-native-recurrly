a953e884b4f443766995c167760ea1f916a4c4f9
E. Hannah <echannah631@gmail.com>
2026-03-30 16:14:44 -0400

    creating app theme in global styles

--- app/index.tsx
+++ app/index.tsx
@@ -3,8 +3,8 @@ import "../global.css";
 
 export default function Index() {
   return (
-    <View className='flex-1 items-center justify-center bg-white'>
-      <Text className='text-xl font-bold text-blue-500'>
+    <View className='flex-1 items-center justify-center bg-background'>
+      <Text className='text-xl font-bold text-success'>
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
