4e4d49fa250134172f2b89e4f99ed5c027c6e703
E. Hannah <echannah631@gmail.com>
2026-03-31 01:06:59 -0400

    implementing fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,41 +8,25 @@ const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
   return (
-    <SafeAreaView className='flex-1 items-center justify-center bg-background'>
-      <Text className='text-xl font-bold text-success'>
-        Edit app/index.tsx to edit this screen.
-      </Text>
+    <SafeAreaView className='flex-1 bg-background p-5'>
+      <Text className='text-primary text-7xl font-sans-extrabold'>Home</Text>
       <Link
         href='/onboarding'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-bold rounded bg-primary text-white p-4'
       >
         Go to Onboarding
       </Link>
       <Link
         href='/(auth)/sign-in'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-bold rounded bg-primary text-white p-4'
       >
         Go to Sign In
       </Link>
       <Link
         href='/(auth)/sign-up'
-        className='mt-4 rounded bg-primary text-white p-4'
+        className='mt-4 font-bold rounded bg-primary text-white p-4'
       >
         Go to Sign Up
-      </Link>
-      <Link
-        href='/subscriptions/spotify'
-        className='mt-4 rounded bg-primary text-white p-4'
-      >
-        Spotify Subscription
-      </Link>
-      <Link
-        href={{
-          pathname: "/subscriptions/[id]",
-          params: { id: "claude" },
-        }}
-      >
-        Claude Max Subscription
       </Link>
     </SafeAreaView>
   );
