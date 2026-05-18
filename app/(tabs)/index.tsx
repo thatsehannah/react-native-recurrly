5d15e2655856ac4a695205404b3eab19d2977bf1
E. Hannah <echannah631@gmail.com>
2026-05-17 23:27:51 -0400

    starting to create home screen components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,33 +1,85 @@
-import { Link } from "expo-router";
+import ListHeading from "@/components/ListHeading";
+import SubscriptionCard from "@/components/SubscriptionCard";
+import UpcomingSubscriptionCard from "@/components/UpcomingSubscriptionCard";
+import {
+  HOME_BALANCE,
+  HOME_SUBSCRIPTIONS,
+  HOME_USER,
+  UPCOMING_SUBSCRIPTIONS,
+} from "@/constants/data";
+import { icons } from "@/constants/icons";
+import images from "@/constants/images";
+import { formatCurrency } from "@/lib/utils";
+import dayjs from "dayjs";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { useState } from "react";
+import { FlatList, Image, Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 // Since the SafeAreaView is a third party component, the styled wrapper enables className support for it
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function Index() {
+  const [expandedSubscriptionId, setExpandedSubscriptionId] = useState<
+    string | null
+  >(null);
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className='text-primary text-7xl font-sans-extrabold'>Home</Text>
-      <Link
-        href='/onboarding'
-        className='mt-4 font-bold rounded bg-primary text-white p-4'
-      >
-        Go to Onboarding
-      </Link>
-      <Link
-        href='/(auth)/sign-in'
-        className='mt-4 font-bold rounded bg-primary text-white p-4'
-      >
-        Go to Sign In
-      </Link>
-      <Link
-        href='/(auth)/sign-up'
-        className='mt-4 font-bold rounded bg-primary text-white p-4'
-      >
-        Go to Sign Up
-      </Link>
+      <View className='home-header'>
+        <View className='home-user'>
+          <Image
+            source={images.avatar}
+            className='home-avatar'
+          />
+          <Text className='home-user-name'>{HOME_USER.name}</Text>
+        </View>
+
+        <Image
+          source={icons.add}
+          className='home-add-icon'
+        />
+      </View>
+
+      <View className='home-balance-card'>
+        <Text className='home-balance-label'>Balance</Text>
+
+        <View className='home-balance-row'>
+          <Text className='home-balance-amount'>
+            {formatCurrency(HOME_BALANCE.amount)}
+          </Text>
+          <Text className='home-balance-date'>
+            {dayjs(HOME_BALANCE.nextRenewalDate).format("MM/DD")}
+          </Text>
+        </View>
+      </View>
+
+      <View>
+        <ListHeading title='Upcoming' />
+        <FlatList
+          data={UPCOMING_SUBSCRIPTIONS}
+          renderItem={({ item }) => <UpcomingSubscriptionCard {...item} />}
+          keyExtractor={(item) => item.id}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          ListEmptyComponent={
+            <Text className='home-empty-state'>No upcoming renewals yet.</Text>
+          }
+        />
+      </View>
+      <View>
+        <ListHeading title='All Subscriptions' />
+        <SubscriptionCard
+          {...HOME_SUBSCRIPTIONS[0]}
+          expanded={expandedSubscriptionId === HOME_SUBSCRIPTIONS[0].id}
+          onPress={() =>
+            setExpandedSubscriptionId((currentId) =>
+              currentId === HOME_SUBSCRIPTIONS[0].id
+                ? null
+                : HOME_SUBSCRIPTIONS[0].id,
+            )
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 }
